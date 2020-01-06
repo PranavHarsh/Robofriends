56e0c7620a3d18c121491be6d4b687b86cd1b490
PranavHarsh <pranavharshbara@gmail.com>
2020-01-06 21:07:08 +0530

    Updates

---
+++ precache-manifest.e2134477669795ca2306c8f3a342582a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fdde7c6e90d59f9b5d0f9c5f5ebe9d",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "09bc4a51c45092bbbfda",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "43f45e361982b5a850ad",
+    "url": "/Robofriends/static/css/main.13c977b8.chunk.css"
+  },
+  {
+    "revision": "09bc4a51c45092bbbfda",
+    "url": "/Robofriends/static/js/2.8bace378.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robofriends/static/js/2.8bace378.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43f45e361982b5a850ad",
+    "url": "/Robofriends/static/js/main.038d0282.chunk.js"
+  },
+  {
+    "revision": "20a4e234794f4ed5044d",
+    "url": "/Robofriends/static/js/runtime-main.1f241086.js"
+  }
+]);
